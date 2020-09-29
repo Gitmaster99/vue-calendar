e42c2c91a6949a7974fbb622e42b92d80668f1ed
Wesley9942 <wvanbalen@gmail.com>
2020-09-29 17:23:51 +0200

    Updated readme file

--- README.md
+++ README.md
@@ -22,3 +22,7 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## Screenshot Calendar App
+```
+![afbeelding](https://user-images.githubusercontent.com/47174750/94578740-5cbaa900-0278-11eb-8d23-b4683e410ce7.png)
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueTextAreaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+Vue.use(VueTextAreaAutosize);
 
 Vue.config.productionTip = false
 
+firebase.initializeApp({
+  apiKey: "AIzaSyB7KViRBEU_hNRgK-oVbR8ltcQyEpQ2h5o",
+    authDomain: "vue-calendar-5e8a6.firebaseapp.com",
+    databaseURL: "https://vue-calendar-5e8a6.firebaseio.com",
+    projectId: "vue-calendar-5e8a6",
+    storageBucket: "vue-calendar-5e8a6.appspot.com",
+    messagingSenderId: "711355683429",
+    appId: "1:711355683429:web:320ffe57d68ee3df75ec8c"
+});
+
+export const db = firebase.firestore();
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
